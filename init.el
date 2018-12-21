606aec70f66344dbdd9a3aa48f46de6aa3430e08
Phillip Oldham <phillip.oldham@gmail.com>
2018-12-21 10:54:45 +0000

    Rebuild config to work with alternative mac Emacs apps
    The Railwaycat version of Emacs broke on Mojave, so this rebuilds
    the config to work with the standard Emacs.app application available
    from homebrew.

--- init.el
+++ init.el
@@ -4,10 +4,31 @@
 ;;; init file which exports a config file from an
 ;;; Org file containing custom settings for Emacs
 
-(eval-when-compile (package-initialize))
+;; Disable writing package settings to init.el
+(defun package--save-selected-packages (&rest opt) nil)
+
+
+(setq package-enable-at-startup t)
+(setq package-archives '(("gnu" . "https://elpa.gnu.org/packages/")
+                         ("melpa-stable" . "https://stable.melpa.org/packages/")
+                         ("melpa" . "https://melpa.org/packages/")
+                         ("org" . "https://orgmode.org/elpa/")))
+
+(setq tls-checktrust t)
+(setq gnutls-verify-error t)
+
+(eval-when-compile
+  (package-initialize))
+(eval-when-compile
+  (if (not (package-installed-p 'req-package))
+    (progn
+      (package-refresh-contents)
+      (package-install 'req-package))))
+
+(require 'req-package)
+
 
 (let ((file-name-handler-alist nil))
-  (setq package-enable-at-startup nil)
   (require 'ob-tangle)
   (setq dotfiles-dir (file-name-directory
                       (or (buffer-file-name) load-file-name)))
@@ -17,3 +38,15 @@
 ;; FIXME - this should be in config.org,
 ;; but doesn't work there
 (setq disabled-command-hook nil)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages (quote (ruby-end el-get req-package))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
